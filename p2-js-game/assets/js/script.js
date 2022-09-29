b3e86b84a11c2e0ef4bc8c036fa39b2dc781f1d7
rjandre <rjandres@bfar.da.gov.ph>
2022-09-30 06:52:54 +0800

    fix : push updates in script.js

--- p2-js-game/assets/js/script.js
+++ p2-js-game/assets/js/script.js
@@ -1,30 +1,58 @@
 // JavaScript Document
-let HolesValue = 7;
+let HolesValue = [7,7,7,7,7,7,7,0,7,7,7,7,7,7,7,0];
 let currentPlayer = 1;
 
 let seedCount;
 
 function loopSeed(){
     let startHole = 3;
-  for(let index = 1; index <= 7; index++){
-      if((index + startHole)==8){
+	let locateHole;
+	let changeStartHole;
+  for(let index = 1; index <= 18; index++){
+
+		  locateHole = startHole;
+	  	  
+	  console.log((index + " " + locateHole + " " + changeStartHole));
+	  
+      if(locateHole==8 || locateHole==16 || locateHole==24 || locateHole==32){
           let home = document.querySelector(`.home.player-${currentPlayer} div`);
-          console.log(home);
-          home.innerHTML = "+1";
-          
-      } else if((index + startHole) > 8){
-          
-          if (currentPlayer==1){
+          //console.log(home);
+		 // console.log((locateHole + " " + changeStartHole));
+		  HolesValue[locateHole-1] += 1;
+          home.innerHTML = locateHole;
+		  home.innerHTML = HolesValue[locateHole-1];
+		  
+		  if (currentPlayer==1){
               currentPlayer = 2;
-          } 
+          } else {
+			  currentPlayer = 1;
+		  }
+		  changeStartHole  = locateHole;
           
-          let hole = document.querySelector(`.player-${currentPlayer}.hole-${(index + startHole)-8}`);
-          console.log(hole);
-          hole.innerHTML = "+1";
+      } else if((locateHole) > 8){
+          let hole = document.querySelector(`.player-${currentPlayer}.hole-${locateHole - changeStartHole}`);
+		  
+		  if((changeStartHole % 16) == 0) {
+			  HolesValue[(locateHole - changeStartHole)-1] += 1;
+			  hole.innerHTML = index + startHole;
+		  hole.innerHTML = HolesValue[(locateHole - changeStartHole)-1];
+		  } else {
+			  HolesValue[(locateHole - changeStartHole)+8] += 1;
+			  hole.innerHTML = index + startHole;
+		  	  hole.innerHTML = HolesValue[(locateHole - changeStartHole)+8];
+		  }
+		  
+		   
+          //console.log((locateHole + " " + changeStartHole));
+          
       } else {
-          let hole = document.querySelector(`.player-${currentPlayer}.hole-${index + startHole}`);
-          hole.innerHTML = "+1";
+          let hole = document.querySelector(`.player-${currentPlayer}.hole-${locateHole}`);
+          HolesValue[locateHole-1] += 1; 
+          //console.log(locateHole);
+          hole.innerHTML = index + startHole;
+		  hole.innerHTML = HolesValue[locateHole-1];
       }
+	  startHole++;
           
   }
       
