63805b41664cb5658cbe1c5667a882909967d928
rjandre <rjandres@bfar.da.gov.ph>
2022-10-03 09:35:53 +0800

    fix : push changes in srcript.js

--- p2-js-game/assets/js/script.js
+++ p2-js-game/assets/js/script.js
@@ -1,95 +1,193 @@
 // JavaScript Document
 let holesValue = [7, 7, 7, 7, 7, 7, 7, 0, 7, 7, 7, 7, 7, 7, 7, 0];
-let currentPlayer = 1;
+let player = 1;
 
 
-
-
+changePlayer(1);
+setHoleValue();
 
 //add Event Listener to holes
-const player1 = Array.from(document.querySelectorAll('.holes.player-1'));
-const player2 = Array.from(document.querySelectorAll('.holes.player-2')).sort().reverse();
-const player1Home = document.querySelector(".home.player-1");
-const player2Home = document.querySelector(".home.player-2");
+function action(index){
+	
+	if (player == 2){
+		if([0,1,2,3,4,5,6].includes(index) == true){
+			console.log("Select to your holes only.");
+			return;
+		}
+		
+		if (holesValue[index] == 0){
+			console.log("Select hole that is not equal to 0.");
+		return;
+		}
+		
+	} else if(player == 1){
 
+		if([8,9,10,11,12,13,14].includes(index) == true){
+			console.log("Select to your holes only.");
+			return;
+		}
+		if (holesValue[index] == 0){
+			console.log("Select hole that is not equal to 0.");
+		return;
+		}
+	}
+	
+	loopSeed(index, player);
+}
 
-const mergeHoles = player1.concat(player1Home , player2, player2Home);
+function changePlayer(currentPlayer){ //changing players turn
+	//console.log(currentPlayer +" ppppppppppppp");
+	const player1 = Array.from(document.querySelectorAll('.holes.player-1'));
+	const player2 = Array.from(document.querySelectorAll('.holes.player-2')).sort().reverse();
+	
+	player1.forEach(function(elem, index) {
+			elem.addEventListener("click", action.bind(this, index), true);
+		});
+	
+	player2.forEach(function(elem, index) {
+			elem.addEventListener("click", action.bind(this, index+8), true);
+		});
+	
+}
 
-player1.forEach(function(elem, index) {
-    elem.addEventListener("click", function() {
-        //this function does stuff
-		alert(elem + " " + elem.innerHTML + " " + index);
-		loopSeed(index);
-    });
-});
-
-
-player2.forEach(function(elem, index) {
-    elem.addEventListener("click", function() {
-        //this function does stuff
-		alert(elem + " " + elem.innerHTML + " " + (index + 8));
-		startHole = index + 8;
-    });
-});
-
-
-console.log(mergeHoles);
-
-setHoleValue();
 
 function setHoleValue()
 {
+	const player1 = Array.from(document.querySelectorAll('.holes.player-1'));
+	const player2 = Array.from(document.querySelectorAll('.holes.player-2')).sort().reverse();
+	const player1Home = document.querySelector(".home.player-1");
+	const player2Home = document.querySelector(".home.player-2");
+
+
+	const mergeHoles = player1.concat(player1Home , player2, player2Home);
+	
 	for(let index = 0; index < holesValue.length; index++)
 		{
 			mergeHoles[index].innerHTML = holesValue[index];
 		}
 }
 
-function checkEmptyHole(currentIndex){
-	if (currentIndex != 7){
-		if(holesValue[currentIndex] != 0){
-            console.log(holesValue[currentIndex]);
-            loopSeed(currentIndex);
-			
-		}
+function checkEmptyHole(currentIndex, currentPlayer){
+	
+	switch (currentIndex) {
+		case 7:
+			break;
+		case 15:
+			break;
+		default:
+			if(holesValue[currentIndex] > 1){
+            	console.log(holesValue[currentIndex]);
+            	loopSeed(currentIndex, currentPlayer);		
+			} else {
+				getOpponentHoleValue(currentIndex);
+				
+				console.log(player + " hjjjjjjjjjjjj");
+				if(player == 1){
+					player=2;
+				} else {
+					player = 1;
+				}
+				console.log(player + " hjjjjjjjjjjjj");
+			}
 	}
-	/**/
 }
 
-function loopSeed(startHole) {
-    let holeLocation;
-    let changeStartHole;
-	let flagMoving = true;
-	let currenIndex;
-	
-	
+
+function loopSeed(startHole, currentPlayer) {
+	let currentIndex;
 	let seedCount = holesValue[startHole];
 	
-	console.log("start hole " +startHole + " " + seedCount);
 	
-	for(let index = 0; index <= seedCount; index++){
+	//console.log("start hole " +startHole + " " + [8,9,10,11,12,13,14].includes(currentIndex) + "   " + currentPlayer);
+	if(currentPlayer == 1){
+	   for(let index = 0; index <= seedCount; index++){
 
-		if(startHole == index + startHole){
-			holesValue[index + startHole] = 0;
-			currenIndex = (index + startHole);
-		}  else{
-            if(currenIndex == 14){
-                currenIndex = 0;
-                holesValue[currenIndex] += 1;
-                //seedCount += 1;
-            } else {
-                currenIndex = (currenIndex + 1);
-                holesValue[currenIndex] += 1;    
-            }  
-        }  
-        console.log("array index " + index + " array index + start " + (index + startHole) +" nnnn hole " + currenIndex + " hole value " + holesValue.length);
-	}
-	console.log("yyyyy" + currenIndex);
-	setHoleValue();
-	//loopSeed(currenIndex);
-    checkEmptyHole(currenIndex);
-}		
+			if(startHole == index + startHole){
+				holesValue[index + startHole] = 0;
+				currentIndex = (index + startHole);
+			}  else{
+				if(currentIndex == 14){
+					currentIndex = 0;
+					holesValue[currentIndex] += 1;
+					//seedCount += 1;
+				} else {
+					currentIndex += 1;
+					holesValue[currentIndex] += 1;    
+				}  
+			}  
+			//console.log("array index " + index + " array index + start " + (index + startHole) +" nnnn hole " + currentIndex + " hole value " + holesValue.length);
+		}
+		//console.log("yyyyy" + currentIndex);
+		setHoleValue();
+		//loopSeed(currentIndex);
+		checkEmptyHole(currentIndex, currentPlayer);
 		
+	   } else if(currentPlayer == 2) {
+		   for(let index = 0; index <= seedCount; index++){
+
+				if(startHole == index + startHole){
+					currentIndex = (index + startHole);
+					holesValue[currentIndex] = 0;		
+				}  else{
+					
+					currentIndex += 1;
+					
+						if(currentIndex == 7){
+							currentIndex = 8;
+							holesValue[currentIndex] += 1; 
+							console.log(currentIndex + "   1 " + index );
+						} else {
+							
+								
+							if(currentIndex == 16){
+								currentIndex = 0;
+								console.log(currentIndex + "    2 " + index);
+								holesValue[currentIndex] += 1;
+								
+							} else {
+								console.log(currentIndex + "   3 " + index);
+								holesValue[currentIndex] += 1;  
+							}
+						   
+				}  
+				//console.log("array index " + index + " array index + start " + (index + startHole) +" nnnn hole " + currentIndex + " hole value " + holesValue.length);
+			}
+		   }
+			console.log("yyyyy " + currentIndex);
+		   	//(currentIndex == 0 ) ? currentIndex = 15 : currentIndex -= 1;
+			setHoleValue();
+			//loopSeed(currentIndex);
+			checkEmptyHole(currentIndex, currentPlayer);
+	   }
+	
+}		
+
+function getOpponentHoleValue(currentIndex){
+	let opponentHole;
+	if([0,1,2,3,4,5,6].includes(currentIndex) == true && player == 1){
+		opponentHole = 14-currentIndex;
+		
+		if(holesValue[opponentHole]!=0) {
+			holesValue[7] = holesValue[7] + holesValue[opponentHole] + holesValue[currentIndex];
+			holesValue[opponentHole] = 0; 
+			holesValue[currentIndex] = 0;
+			console.log("kimarrrr P2 " + opponentHole + " i " + currentIndex);
+		}
+		
+	} else if([8,9,10,11,12,13,14].includes(currentIndex) == true && player == 2){
+		opponentHole = 14 - currentIndex;
+		
+		if(holesValue[opponentHole]!=0) {
+			holesValue[15] = holesValue[15] + holesValue[opponentHole] + holesValue[currentIndex];
+			holesValue[opponentHole] = 0; 
+			holesValue[currentIndex] = 0;
+			console.log("kimarrrr P1 " + opponentHole + " i " + currentIndex);
+		}
+
+	}
+	setHoleValue();
+	
+}
 		
         /*holeLocation = index + startHole;
 		
@@ -103,20 +201,20 @@ function loopSeed(startHole) {
         if(holeLocation < 8){
 
             holesValue[holeLocation-1] += 1;
-			currenIndex = holeLocation;
+			currentIndex = holeLocation;
 			
         } else if ((holeLocation) > 8) {
                 
                 if ((changeStartHole % 16) == 0) {
 
                     holesValue[(holeLocation - changeStartHole) - 1] += 1;
-					currenIndex = (holeLocation - changeStartHole) - 1;
+					currentIndex = (holeLocation - changeStartHole) - 1;
 					console.log(holeLocation + "------------ " + changeStartHole + "===========" + ((holeLocation - changeStartHole)+7));
                     
                 } else {
 
                     holesValue[holeLocation-1] += 1;
-					currenIndex = (holeLocation-1);
+					currentIndex = (holeLocation-1);
  					console.log(holeLocation + "------------ " + changeStartHole + "===========" + ((holeLocation - changeStartHole)+7));
                 }
 
@@ -124,7 +222,7 @@ function loopSeed(startHole) {
 			
 				console.log("home hole "  + (holeLocation-1));
 				holesValue[holeLocation - 1] += 1;
-				currenIndex = holeLocation - 1;	
+				currentIndex = holeLocation - 1;	
 			
                 if (currentPlayer == 1) {
                     currentPlayer = 2;
@@ -139,9 +237,9 @@ function loopSeed(startHole) {
 	changeStartHole = 0;
 	setHoleValue();
 	
-	console.log(currenIndex +"  ddd  "+ currentPlayer);
+	console.log(currentIndex +"  ddd  "+ currentPlayer);
 	if(flagMoving == false){
-		checkEmptyHole(currenIndex + 1);	*/
+		checkEmptyHole(currentIndex + 1);	*/
 
 
 
