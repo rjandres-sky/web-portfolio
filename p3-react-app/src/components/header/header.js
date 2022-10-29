e6cb78e04349cf596c431d782fcce40e92edcee1
rjandre <rjandres@bfar.da.gov.ph>
2022-10-29 15:10:29 +0800

    feat : add header and footer

---
+++ p3-react-app/src/components/header/header.js
@@ -0,0 +1,17 @@
+
+import './header.css';
+
+const Header = () => {
+
+    return (
+        <>
+        <nav> 
+            <h1> Document Tracking</h1>
+            
+            <h4 className='user-welcome'> Welcome : User | <a href='#'>Login</a></h4>
+        </nav>
+        </>
+    )
+}
+
+export default Header;
