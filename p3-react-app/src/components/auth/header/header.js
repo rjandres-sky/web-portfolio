30df78a8bcd02ff6e1b9c85471a973883d204371
rjandre <rjandres@bfar.da.gov.ph>
2022-11-02 10:56:56 +0800

    feat : add User Authentication

---
+++ p3-react-app/src/components/auth/header/header.js
@@ -0,0 +1,15 @@
+
+import './auth-header.css';
+
+const LoginHeader = () => {
+
+    return (
+        <>
+        <nav  className='login-header'> 
+            <h1> Document Tracking</h1>
+        </nav>
+        </>
+    )
+}
+
+export default LoginHeader;
