0332fa86cc3e63b3e98c5cbf51b265f4eab1f61e
rjandre <rjandres@bfar.da.gov.ph>
2022-11-02 11:01:22 +0800

    fix : add menu for Administrator

--- p3-react-app/src/components/header/header.js
+++ p3-react-app/src/components/header/header.js
@@ -1,15 +1,31 @@
 
+import { useDispatch, useSelector } from 'react-redux';
 import './header.css';
 
 const Header = () => {
+    const dispatch = new useDispatch();
+    const user = useSelector(state => state.auth);
+
+    const logoutEventHandler = () => {
+        dispatch({ type: 'LOGOUT' })
+    }
 
     return (
         <>
-        <nav> 
-            <h1> Document Tracking</h1>
-            
-            <h4 className='user-welcome'> Welcome : User | <a href='#'>Login</a></h4>
-        </nav>
+            <nav>
+                <h1> Document Tracking</h1>
+                { user[0].roletype === 'Administrator' &&
+                    <>
+                        <ul>
+                            <li> | Division </li>
+                            <li> | Users </li>
+                            <li> | Documents </li>
+
+                        </ul>
+                    </>
+                }
+                <h4 className='user-welcome'>  <span> Welcome : { user[0].name.toUpperCase() } </span>  <p onClick={logoutEventHandler}>Log out</p></h4>
+            </nav>
         </>
     )
 }
