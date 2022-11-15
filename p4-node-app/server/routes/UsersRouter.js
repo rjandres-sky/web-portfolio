04bf8f10f085d8a50d0c48fee50465647ffd178a
rjandre <rjandres@bfar.da.gov.ph>
2022-11-15 22:55:22 +0800

    feat : added change password route

--- p4-node-app/server/routes/UsersRouter.js
+++ p4-node-app/server/routes/UsersRouter.js
@@ -1,55 +1,76 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+
 const users = require('../models/UsersModel');
 
 router.get('/', (request, response) => {
     users.find()
-    .then(
-        result => {
-            response.send(result)        
-        })
-        console.log(users)
+        .then(
+            result => {
+                response.send(result)
+            })
+    console.log(users)
 })
 
 router.get('/:id', (request, response) => {
     users.findOne({ "sections.employees._id": request.params.id })
-    .then(
-        result => {
-            console.log(result)
-            response.send(result)        
-        })
-        console.log(users)
+        .then(
+            result => {
+                console.log(result)
+                response.send(result)
+            })
+    console.log(users)
 })
 
 router.post('/', async (request, response) => {
-    users.create(request.body)
-    .then( result => {
-            response.send({ status: "New User has been added", result : result });
-    });
+    await bcrypt.hash(request.body.password, 10)
+        .then(hashedPassword =>
+            users.create({ ...request.body, password: hashedPassword })
+                .then(result => {
+                    response.send({ status: "New User has been added", result: result });
+                })
+        )
+
 })
 
-router.put('/:id', ( request, response ) => {
-    users.updateOne( 
-        { _id:request.params.id}, 
-        { $set: { "sections.$.employees.$[emp]" : request.body } })
-    .then( result => {
-            if(result.modifiedCount === 1){ 
-                response.send({ status: "User has been updated", result : result});
-            }
-            
-    });
+//change password
+router.put('/:id/change', async(request, response) => {
+    await bcrypt.hash(request.body.password, 10)
+        .then(hashedPassword =>
+            users.updateOne(
+            { _id: request.params.id },
+            { $set: { password: hashedPassword } })
+            .then(result => {
+                if (result.modifiedCount === 1) {
+                    response.send({ status: "Password changed", result: result });
+                }
+            })
+        )
 });
 
-router.delete('/:id', ( request, response ) => {
+router.put('/:id', (request, response) => {
+    users.updateOne(
+        { _id: request.params.id },
+        { $set: { "sections.$.employees.$[emp]": request.body } })
+        .then(result => {
+            if (result.modifiedCount === 1) {
+                response.send({ status: "User has been updated", result: result });
+            }
+
+        });
+});
+
+router.delete('/:id', (request, response) => {
     users.deleteOne({ _id: request.params.id })
-    .then( result => {
-        if( result.deletedCount === 1 ){
-            response.send({
-                status: "Comment has been deleted"
-            });
-        }
-    });
+        .then(result => {
+            if (result.deletedCount === 1) {
+                response.send({
+                    status: "Comment has been deleted"
+                });
+            }
+        });
 });
 
 module.exports = router;
