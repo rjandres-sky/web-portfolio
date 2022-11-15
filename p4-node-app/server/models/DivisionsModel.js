12901077f533f41f9fa5c4df739efefc71b67202
rjandre <rjandres@bfar.da.gov.ph>
2022-11-15 22:58:06 +0800

    fix : remove section schema

--- p4-node-app/server/models/DivisionsModel.js
+++ p4-node-app/server/models/DivisionsModel.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const SectionsSchema = new Schema ({
-    section_name : String,
-    description : String,
-})
-
 const DivisionsSchema = new Schema ({
     division : String,
     description : String,
-    sections : [SectionsSchema]
 })
 
 module.exports = mongoose.model('divisions', DivisionsSchema);
