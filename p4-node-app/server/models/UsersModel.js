b0b0d468f52a5e3658938f4e6eb4fb7cdcdebcc2
rjandre <rjandres@bfar.da.gov.ph>
2022-11-15 13:53:33 +0800

    feat : add UserModel

---
+++ p4-node-app/server/models/UsersModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EmployeesSchema = new Schema ({
+    empid : String,
+    name : {lastname : String, firstname: String, middlename: String},
+    position : String,
+    section : {type : Schema.Types.ObjectId, ref='divisions.sections'},
+    username : String,
+    password : String,
+    role : String,
+    divionhead : Boolean,
+    sectionhead : Boolean,
+    isactive : Boolean
+})
+
+module.exports = mongoose.model('users', UsersSchema);
