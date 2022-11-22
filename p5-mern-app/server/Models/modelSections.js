259ca4e958647c3ab9da6881baa7863e794e55c9
rjandre <rjandres@bfar.da.gov.ph>
2022-11-22 11:27:15 +0800

    fix : add unique key validator

--- p5-mern-app/server/Models/modelSections.js
+++ p5-mern-app/server/Models/modelSections.js
@@ -1,10 +1,11 @@
 const mongoose  = require("mongoose");
+const mongooseUniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema
 
 const SectionsSchema = new Schema({
     division : {type : Schema.Types.ObjectId, ref : 'divisions', required : true},
-    section : { type : String, required : true},
+    section : { type : String, required : true, unique : true},
     sectionDesc : { type : String, required : true}
 })
-
+SectionsSchema.plugin(mongooseUniqueValidator)
 module.exports = mongoose.model('sections', SectionsSchema)
