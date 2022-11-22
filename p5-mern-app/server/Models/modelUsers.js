3c97f19faebddec8e8e60d023860c0a9597027c2
rjandre <rjandres@bfar.da.gov.ph>
2022-11-22 16:37:31 +0800

    fix : remove returning password in Auth API

--- p5-mern-app/server/Models/modelUsers.js
+++ p5-mern-app/server/Models/modelUsers.js
@@ -1,23 +1,23 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 const mongooseUniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema
 
-const UsersSchema = new Schema ({
-    employee_id : { type : String, require: true, unique : true},
-    name : {
-        lastname : { type : String, require: true},
-        firstname : { type : String, require: true},
-        middlename : { type : String},
-        extension :  { type : String},
+const UsersSchema = new Schema({
+    employee_id: { type: String, require: true, unique: true },
+    name: {
+        lastname: { type: String, require: true },
+        firstname: { type: String, require: true },
+        middlename: { type: String },
+        extension: { type: String },
     },
-    position : { type : String, require: true},
-    username : { type : String, require: true, unique : true},
-    password : { type : String, require: true},
-    division : { type : Schema.Types.ObjectId, ref : 'divisions', require: true},
-    section : { type : Schema.Types.ObjectId, ref : 'sections', require: true},
-    isDivisionHead : {type : Boolean, default : false},
-    isSectionHead : {type : Boolean, default : false},
-    isAdmin : {type : Boolean, default : false}
+    position: { type: String, require: true },
+    username: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    division: { type: Schema.Types.ObjectId, ref: 'divisions', require: true },
+    section: { type: Schema.Types.ObjectId, ref: 'sections', require: true },
+    isDivisionHead: { type: Boolean, default: false },
+    isSectionHead: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false }
 })
 UsersSchema.plugin(mongooseUniqueValidator)
 
--- p5-mern-app/server/Routers/routeAuth.js
+++ p5-mern-app/server/Routers/routeAuth.js
@@ -5,18 +5,19 @@ const bcrypt = require('bcrypt');
 const User = require('../Models/modelUsers');
 
 router.post('/', (request, response) => {
-    User.findOne({ username: request.body.username })
-        .select({ password: 0 })
-        .populate({ path: 'division', select: { sections: 0 } })
-        .populate({ path: 'section', select: { division: 0 } })
+    User.findOne({$and : [{ username: request.body.username }]})
         .then(result => {
             if (result !== null) {
                 bcrypt.compare(request.body.password, result.password, (err, match) => {
                     if (match) {
                         // Autheticated, valid email and password
-                        response.status(200).send(result);
+                        User.findOne({_id : result._id})
+                        .select({password : 0})
+                        .populate({ path: 'division', select: { sections: 0 } })
+                        .populate({ path: 'section', select: { division: 0 } })
+                        .then(result => response.status(200).send(result))
                     } else {
-                        response.status(400).send('Invalid Username or Password')
+                        response.status(400).send('Invalid Username or Password2')
                     }
                 });
             }
